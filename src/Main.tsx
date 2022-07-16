e576779cd24ba57d95ca52fb808489e734871469
Cory House <housecor@gmail.com>
2022-07-16 14:12:34 -0500

    Add main

---
+++ src/Main.tsx
@@ -0,0 +1,13 @@
+import Button from "./Button";
+
+export default function Main() {
+  return (
+    <>
+      <p>
+        This merely exists to display the components. This file isn't bundled in
+        the library.
+      </p>
+      <Button />
+    </>
+  );
+}
