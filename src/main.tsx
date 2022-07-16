fa9356aaf52720547bdfeee6e953f9d324a3679a
Cory House <housecor@gmail.com>
2022-07-16 14:14:47 -0500

    Fix entry

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import Button from "./Button";
 
-export default function Main() {
-  return (
-    <>
-      <p>
-        This merely exists to display the components. This file isn't bundled in
-        the library.
-      </p>
-      <Button />
-    </>
-  );
-}
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <p>
+      This file merely exists to display the components. This file isn't bundled
+      in the library.
+    </p>
+    <Button />
+  </React.StrictMode>
+);
