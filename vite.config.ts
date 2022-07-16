d51cb7f746e0186ca106d5f30b21ccf3ec6aa5c2
Cory House <housecor@gmail.com>
2022-07-16 15:11:34 -0500

    Add visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,16 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      open: true,
+    }),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
