5fd0547229b8b660f4993bb0abe9f67c79e9ed30
Cory House <housecor@gmail.com>
2022-09-24 14:15:34 -0500

    Configure plugin

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@
 
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
 
   test: {
     globals: true,
